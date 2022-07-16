fbd7086c6ce7d0299d6724a1179a495dac1c614c
knorth <kayci.north@gmail.com>
2022-07-16 10:27:17 -0600

    linked image to card using dataset

--- js/app.js
+++ js/app.js
@@ -8,7 +8,7 @@
 //Game play: several functions with conditionals. create array to hold value of 2 cards that are flipped. If they match, the appropriate player's score is increased and it's the next player's turn. If they don't match, the next player goes. Players turn variable is increased next player is up, use setTimeout method.
 //Make conditionals to see if the whole board is cleared/matched, if so declare the winner. 
 //If not a match, Make function to flip card back over targeting 2 card array? 
-//How to determine players turn it is???  If playersTurn % 2 === 0, playerID = 1  else playerID = 2
+//How to determine players turn it is???  If playersTurn % 2 === 0
 //reset button add eventListener function
 //make start/play button function with event listener that asks for names for both players, add conditionals etc.
 //enable start button once clicked.
@@ -18,9 +18,10 @@
 const cardsArray = ['🍕', '🥐', '🍷', '🍯', '🍦', '🦪', '🥟', '🌯', '🥓', '🍄', '🥑', '🫒'];
 
 //global variables
-let firstGuess = ""
-let secondGuess = ""
+let firstGuess = ''
+let secondGuess = ''
 let nextTurn = 0
+let previousCard = null
 let player1Score = 1
 let player2Score = 1
 
@@ -45,6 +46,7 @@ gameGrid.forEach(output => {
 
 const card = document.createElement('div')
 card.setAttribute('class', 'card')
+card.dataset.img = output  //link the image to the card using dataset to the shuffled array targeting output variable.
 
 const front = document.createElement('div')
 front.setAttribute('class', 'front')
@@ -83,7 +85,7 @@ function resetCards(){
 
 grid.addEventListener('click', event =>{
     const clicked = event.target
-    if(clicked.selected === ??????????)
+    // if(clicked.selected === )   //
 })
 
 
